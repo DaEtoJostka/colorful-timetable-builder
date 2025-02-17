b4fd87255d98ef17d2b31669858dbe72d057c20f
Ivan Karlov <karlovivan679@gmail.com>
2025-02-17 20:22:15 +0500

    Configure Grunt for project build process
    - Add Grunt as a development dependency
    - Install grunt-cli globally in GitHub Actions workflow
    - Update npm-grunt.yml to install dependencies and run Grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Add your grunt tasks here
+  });
+
+  // Load plugins here
+  
+  // Default task(s)
+  grunt.registerTask('default', []);
+}; 
